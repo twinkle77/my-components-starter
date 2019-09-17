d0fed9bd7b54f6675d119372f779fd78fd3e1bc1
twinkle77 <2153165927@qq.com>
2019-09-17 17:32:27 +0800

    refactor: 跑通流程

--- examples/main.js
+++ examples/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import Jdm from '../src/main.js'
+
+Vue.use(Jdm)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ packages/component/src/main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>test - component</div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent'
+}
+</script>
+
+<style lang="less" scoped>
+div {
+  color: yellow;
+}
+</style>
